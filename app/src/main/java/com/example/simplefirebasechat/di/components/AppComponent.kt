c4648f743760042d2ae57ff91c1f1772bf9622e6
DmitryDanilov <d.danilov@vironit.co.uk>
2019-12-11 17:23:25 +0300

    >AppComponent

--- app/src/main/java/com/example/simplefirebasechat/di/components/AppComponent.kt
+++ app/src/main/java/com/example/simplefirebasechat/di/components/AppComponent.kt
@@ -1,6 +1,7 @@
 package com.example.simplefirebasechat.di.components
 
 import android.content.Context
+import android.content.SharedPreferences
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
@@ -8,6 +9,8 @@ import javax.inject.Singleton
 @Singleton
 @Component
 interface AppComponent {
+    fun registerSignInComponent(): SignInComponent
+    fun inject(): SharedPreferences
     @Component.Factory
     interface Factory {
         fun create(@BindsInstance context: Context): AppComponent
