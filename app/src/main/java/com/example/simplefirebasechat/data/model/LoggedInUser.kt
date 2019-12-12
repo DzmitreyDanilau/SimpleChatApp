b6d4012bea3154fcf352a4a49ea6188ec3ed8b21
DmitryDanilov <d.danilov@vironit.co.uk>
2019-12-12 17:31:17 +0300

    >auth_nav_grapg +GoogleSignIn

--- app/src/main/java/com/example/simplefirebasechat/data/model/LoggedInUser.kt
+++ app/src/main/java/com/example/simplefirebasechat/data/model/LoggedInUser.kt
@@ -1,9 +1,14 @@
 package com.example.simplefirebasechat.data.model
 
+import java.io.Serializable
+
 /**
  * Data class that captures user information for logged in users retrieved from LoginRepository
  */
 data class LoggedInUser(
     val userId: String,
-    val displayName: String
-)
+    val displayName: String,
+    val uid: String,
+    val email: String,
+    val isAuthenticated: Boolean
+) : Serializable
